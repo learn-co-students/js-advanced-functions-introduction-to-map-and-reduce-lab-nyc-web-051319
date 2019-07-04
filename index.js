617ab8ce048d0666afc6918d09951043059de0e8
github username <github email address>
2019-07-04 15:42:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function mapToNegativize(arr){
+  return arr.map(x => x * -1)
+}
+
+function mapToNoChange(arr){
+  return arr.map(x => x)
+}
+
+function mapToDouble(arr){
+  return arr.map(x => x * 2)
+}
+
+function mapToSquare(arr){
+  return arr.map(x => Math.pow(x, 2))
+}
+
+function reduceToTotal(arr, startingPoint = 0){
+  let total = startingPoint;
+  for (let i = 0; i < arr.length; i++){
+    total += arr[i]
+  }
+  return total;
+}
+
+function reduceToAllTrue(arr){
+  let isTrue = true
+  for (let i = 0; i < arr.length; i++){
+    if (!arr[i]){
+      isTrue = false
+      return isTrue
+    }
+  }
+  return isTrue
+}
+
+function reduceToAnyTrue(arr){
+  let isFalse = false
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i]){
+      isFalse = true;
+      return isFalse
+    }
+  }
+  return isFalse;
+}
