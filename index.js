520e96c1c8045aabd257bfc2fa411b388e9b3375
chrisyuaners <chrisyuaners@gmail.com>
2019-07-05 22:14:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  const newArray = []
+  sourceArray.forEach(element => {
+    newArray.push(-element)
+  })
+  return newArray
+}
+
+function mapToNoChange(sourceArray) {
+  return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+  const newArray = []
+  sourceArray.forEach(element => {
+    newArray.push(element * 2)
+  })
+  return newArray
+}
+
+function mapToSquare(sourceArray) {
+  const newArray = []
+  sourceArray.forEach(element => {
+    newArray.push(element ** 2)
+  })
+  return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let total = startingPoint
+  sourceArray.forEach(element => {
+    total += element
+  })
+  return total
+}
+
+function reduceToAllTrue(sourceArray) {
+  const newArray = []
+  const arrayLength = sourceArray.length
+  sourceArray.forEach(element => {
+    if (element) {
+      newArray.push(element)
+    }
+  })
+  if (newArray.length === arrayLength) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function reduceToAnyTrue(sourceArray) {
+  const newArray = []
+  const arrayLength = sourceArray.length
+  sourceArray.forEach(element => {
+    if (element) {
+      newArray.push(element)
+    }
+  })
+  if (newArray.length > 0) {
+    return true
+  } else {
+    return false
+  }
+}
